b368d9ac2eb5698ba94aea00f01006d75da4f7bf
William Cole <madbillyblack@gmail.com>
2021-10-09 23:46:45 +0200

    Began laying code foundation for PRESSURE CHIEF lockdown script. Created Sector Class to conatain all components.  Created list for all compnent types. TagsFromName() to get sector tag from block name. Began work on build function which runs on first load and refreshes and builds network.

--- PlanetMap_3D/Script.cs
+++ PlanetMap_3D/Script.cs
@@ -794,6 +794,8 @@ public void Main(string argument)
 			
 			// We are done with the frame, send all the sprites to the text panel
 			_frame.Dispose();
+			
+			_statusMessage = Me.DefinitionDisplayName;
 		}
 	}
 	else
---
+++ Pressure_Chief/Script.cs
@@ -0,0 +1,138 @@
+﻿//////////////////////////////////
+// PRESSURE CHIEF			  	//
+// Pressure management script	//
+// Author: SJ_Omega				//
+//////////////////////////////v1.0
+
+// USER CONSTANTS -  Feel free to change as needed:
+
+
+
+// AVOID CHANGING ANYTHING BELOW THIS LINE!!!!!------------------------------------------------------------------------------------------------
+const string OPENER = "[|";
+const string CLOSER = "|]";
+const string SPLITTER = "|";
+
+// Globals //
+string _statusMessage;
+
+List<IMyAirVent> _vents;
+List<IMyDoor> _doors;
+List<IMyTextPanel> _lcds;
+List<IMySoundBlock> _lockAlarms;
+List<IMyTimerBlock> _lockTimers;
+List<IMyShipConnector> _connectors;
+List<IMyShipMergeBlock> _mergeBlocks;
+List<IMyLightingBlock> _lights;
+List<Sector> _sectors;
+
+
+
+public class Sector{
+	
+	public string tag;
+	public IMyAirVent vent;
+	public List<IMyDoor> doors;
+	public List<IMyLightingBlock> lights;
+	public string type; // Room, Lock, or Dock
+	public IMyTimerBlock lockTimer;
+	public IMySoundBlock lockAlarm;
+	public IMyShipConnector connector;
+	
+	public Sector(IMyAirVent airVent){
+		this.vent = airVent;
+		this.tag = TagsFromName(airVent.CustomName);
+	}
+}
+
+
+public Program()
+{
+	_statusMessage = "";
+	Build();
+	Runtime.UpdateFrequency = UpdateFrequency.Update100;
+}
+
+public void Save()
+{
+}
+
+public void Main(string argument, UpdateType updateSource)
+{
+	if(_sectors.Count < 1){
+		Echo("NOTHING TO SEE");
+		return;
+	}
+		
+	foreach(Sector sector in _sectors){
+		Echo(sector.tag);
+	}
+}
+
+
+// TAGS FROM NAME //
+public static string TagsFromName(string name){
+	
+	int start = name.IndexOf(OPENER) + OPENER.Length; //Start index of tag substring
+	int length = name.IndexOf(CLOSER) - start; //Length of tag
+	
+	return name.Substring(start, length);
+}
+
+
+// BUILD // Searches grid for all components and adds them to current run.
+void Build(){
+	_vents = new List<IMyAirVent>();
+	_doors = new List<IMyDoor>();
+	_lcds = new List<IMyTextPanel>();
+	_lockAlarms = new List<IMySoundBlock>();
+	_lockTimers = new List<IMyTimerBlock>();
+	_connectors = new List<IMyShipConnector>();
+	_mergeBlocks = new List<IMyShipMergeBlock>();
+	_lights = new List<IMyLightingBlock>();
+	_sectors = new List<Sector>();
+	
+	
+	List<IMyTerminalBlock> blocks = new List<IMyTerminalBlock>();
+	GridTerminalSystem.SearchBlocksOfName(OPENER, blocks);
+	
+	if(blocks.Count > 0){
+		foreach(IMyTerminalBlock block in blocks){
+			switch(block.BlockDefinition.TypeIdString){
+				case "MyObjectBuilder_AirVent":
+					_vents.Add(block as IMyAirVent);
+					break;
+				case "MyObjectBuilder_Door":
+					_doors.Add(block as IMyDoor);
+					break;
+				case "MyObjectBuilder_TextPanel":
+					_lcds.Add(block as IMyTextPanel);
+					break;
+				case "MyObjectBuilder_SoundBlock":
+					_lockAlarms.Add(block as IMySoundBlock);
+					break;
+				case "MyObjectBuilder_TimerBlock":
+					_lockTimers.Add(block as IMyTimerBlock);
+					break;
+				case "MyObjectBuilder_ShipConnector":
+					_connectors.Add(block as IMyShipConnector);
+					break;
+				case "MyObjectBuilder_MergeBlock":
+					_mergeBlocks.Add(block as IMyShipMergeBlock);
+					break;
+				case "MyObjectBuilder_InteriorLight":
+				case "MyObjectBuilder_ReflectorLight":
+					_lights.Add(block as IMyLightingBlock);
+					break;
+			}
+		}
+		
+		
+		foreach(IMyAirVent vent in _vents){
+			Sector sector = new Sector(vent);
+			_sectors.Add(sector);
+		}
+	}
+	
+
+}
