974e0ce4215ad445b66b736e0dc08076e6e35a04
William Cole <madbillyblack@gmail.com>
2021-10-10 23:35:38 +0200

    Added empty Assign functions for all relevent block types.  Cannot make generalized function because they need to be attached to specific parameters for each sector.  Completed basics of AssignDoor().  Assigns door to both sectors designated in name.

--- Pressure_Chief/Script.cs
+++ Pressure_Chief/Script.cs
@@ -8,13 +8,14 @@
 
 
 
-// AVOID CHANGING ANYTHING BELOW THIS LINE!!!!!------------------------------------------------------------------------------------------------
+// AVOID CHANGING ANYTHING BELOW THIS LINE!!!!!-----------------------------------------------------------------------------------------------------
 const string OPENER = "[|";
 const string CLOSER = "|]";
-const string SPLITTER = "|";
+const char SPLITTER = '|';
+const string INI_HEAD = "Pressure Chief";
 
 // Globals //
-string _statusMessage;
+static string _statusMessage;
 
 List<IMyAirVent> _vents;
 List<IMyDoor> _doors;
@@ -28,13 +29,15 @@ List<Sector> _sectors;
 
 
 
+// CLASSES //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 public class Sector{
 	
 	public string tag;
 	public IMyAirVent vent;
 	public List<IMyDoor> doors;
 	public List<IMyLightingBlock> lights;
-	public string type; // Room, Lock, or Dock
+	public string type; // User Defined Defaults to "Room"
 	public IMyTimerBlock lockTimer;
 	public IMySoundBlock lockAlarm;
 	public IMyShipConnector connector;
@@ -42,6 +45,8 @@ public class Sector{
 	public Sector(IMyAirVent airVent){
 		this.vent = airVent;
 		this.tag = TagsFromName(airVent.CustomName);
+		this.doors = new List<IMyDoor>();
+		this.lights = new List<IMyLightingBlock>();
 	}
 }
 
@@ -57,6 +62,8 @@ public void Save()
 {
 }
 
+
+// MAIN /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 public void Main(string argument, UpdateType updateSource)
 {
 	if(_sectors.Count < 1){
@@ -65,7 +72,12 @@ public void Main(string argument, UpdateType updateSource)
 	}
 		
 	foreach(Sector sector in _sectors){
-		Echo(sector.tag);
+		Echo(sector.type + " " + sector.tag);
+		if(sector.doors.Count > 0){
+			foreach(IMyDoor door in sector.doors)
+				Echo("* " + door.CustomName);
+		}
+		Echo(" ");
 	}
 }
 
@@ -79,6 +91,22 @@ public static string TagsFromName(string name){
 	return name.Substring(start, length);
 }
 
+
+// MULTITAGS // Returns 2 entry array of tags for blocks with multiple tags.
+public static string[] MultiTags(string name){
+	string bigTag = TagsFromName(name);
+	string[] output = bigTag.Split(SPLITTER);
+	
+	if(output.Length == 2)
+		return output;
+	
+	_statusMessage = "Split Error for " + name;
+	return new string[]{"ERROR", "ERROR"};
+}
+
+
+
+// BUILD FUNCIONS -----------------------------------------------------------------------------------------------------------------------------------
 
 // BUILD // Searches grid for all components and adds them to current run.
 void Build(){
@@ -127,12 +155,85 @@ void Build(){
 			}
 		}
 		
-		
 		foreach(IMyAirVent vent in _vents){
 			Sector sector = new Sector(vent);
+			sector.type = GetKey(vent, "Sector_Type", "Room");
 			_sectors.Add(sector);
 		}
+		
+		if(_sectors.Count < 1 || _doors.Count < 1)
+			return;
+		
+		foreach(IMyDoor door in _doors)
+			AssignDoor(door);		
 	}
-	
+}
 
+
+// ASSIGN DOOR //
+void AssignDoor(IMyDoor door){
+	string[] tags = MultiTags(door.CustomName);
+	
+	foreach(Sector sector in _sectors){
+		if(sector.tag == tags[0] || sector.tag == tags[1])
+			sector.doors.Add(door);
+	}
+}
+
+
+// ASSIGN LCD //
+void AssignLCD(IMyTextPanel lcd){}
+
+
+// ASSIGN ALARM //
+void AssignAlarm(IMySoundBlock alarm){}
+
+
+// ASSIGN TIMER //
+void AssignTimer(IMyTimerBlock timer){}
+
+
+// ASSIGN CONNECTOR //
+void AssignConnector(IMyShipConnector connector){}
+
+
+// ASSIGN MERGE BLOCK //
+void AssignMergeBlock(IMyShipMergeBlock mergeBlock){}
+
+
+// ASSIGN LIGHT //
+void AssignLight(IMyLightingBlock light){}
+
+
+
+// INI FUNCTIONS -----------------------------------------------------------------------------------------------------------------------------------
+
+// GET KEY // Gets ini value from block.  Returns default argument if doesn't exist.
+string GetKey(IMyTerminalBlock block, string key, string defaultVal){
+	if(!block.CustomData.Contains(INI_HEAD)||!block.CustomData.Contains(key))
+		SetIni(block, key, defaultVal);
+	
+	MyIni blockIni = GetIni(block);
+	
+	return blockIni.Get(INI_HEAD, key).ToString();
+}
+
+
+// SET INI // Update ini key for block, and write back to custom data.
+void SetIni(IMyTerminalBlock block, string key, string arg){
+	MyIni blockIni = GetIni(block);
+	blockIni.Set(INI_HEAD, key, arg);
+	block.CustomData = blockIni.ToString();
+}
+
+
+// GET INI //
+MyIni GetIni(IMyTerminalBlock block){
+	MyIni iniOuti = new MyIni();
+	
+	MyIniParseResult result;
+	if (!iniOuti.TryParse(block.CustomData, out result)) 
+		throw new Exception(result.ToString());
+	
+	return iniOuti;
 }
