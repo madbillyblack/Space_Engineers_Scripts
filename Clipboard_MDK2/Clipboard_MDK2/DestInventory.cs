802654877029191bbb3d6f12fe63581175ca4063
William Cole <madbillyblack@gmail.com>
2026-06-29 20:55:11 +0200

    Migrated Clipboard Script to MDK2
    * Created updated version of inventory management systems

---
+++ Clipboard_MDK2/Clipboard_MDK2/DestInventory.cs
@@ -0,0 +1,170 @@
+﻿using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using VRage;
+using VRage.Collections;
+using VRage.Game;
+using VRage.Game.Components;
+using VRage.Game.GUI.TextPanel;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ModAPI.Ingame.Utilities;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRageMath;
+
+namespace IngameScript
+{
+    partial class Program
+    {
+        class DestInventory
+        {
+
+            public DestType Type;
+            public Dictionary<string, MyItemType> ItemTypes;
+            public Dictionary<string, int> ItemAmmounts;
+            public MyIniHandler IniHandler;
+            public IMyInventory Inventory;
+            public IMyTerminalBlock Block;
+
+            public DestInventory(DestType type, IMyTerminalBlock block)
+            {
+                Type = type;
+                Block = block;
+                IniHandler = new MyIniHandler(Block);
+                Inventory = Block.GetInventory();
+                ItemTypes = new Dictionary<string, MyItemType>();
+                ItemAmmounts = new Dictionary<string, int>();
+            }
+
+            public void SetLoadout()
+            {
+                List<MyItemType> itemTypes = new List<MyItemType>();
+                Inventory.GetAcceptedItems(itemTypes);
+
+                string [] keyList = IniHandler.GetKey(INI_HEAD, LOADOUT, "").Split('\n');
+
+                if(keyList.Length < 1) { return; }
+
+                foreach (string keyItem in keyList)
+                {
+                    AddTypeAndCount(keyItem, itemTypes);
+                }
+            }
+
+            public bool Refill(List<IMyInventory> sources)
+            {
+                if(ItemTypes.Keys.Count < 1) { return true; }
+
+                int unstocked = 0;
+
+                foreach (string key in ItemTypes.Keys)
+                {
+                    int targetCount = ItemAmmounts[key];
+                    MyItemType itemType = ItemTypes[key];
+                    bool restocked = false;
+
+                    if (targetCount > 0)
+                    {
+                        foreach (IMyInventory source in sources)
+                        {
+                            if(!restocked)
+                            {
+                                restocked = RestockItem(itemType, targetCount, source);
+
+                                int missingCount = targetCount - Inventory.GetItemAmount(itemType).ToIntSafe();
+
+                                if (missingCount > 0)
+                                {
+                                    if(Inventory.CanItemsBeAdded(missingCount, itemType))
+                                    {
+                                        // Had some but not enough of the needed item.
+                                        restocked = false;
+                                    }
+                                    else
+                                    {
+                                        _surface.WriteText("-- WARNING: The requested amount for item " + key + "\n in " + Block.CustomName + " exceeds capacity!\n");
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        restocked = true;
+                    }
+
+                    if (!restocked)
+                        unstocked++;
+                }
+
+                // If all inventories fully stocked, return true
+                return unstocked < 1;
+            }
+
+            private bool RestockItem(MyItemType itemType, int targetAmmount, IMyInventory source)
+            {
+                int currentCount = Inventory.GetItemAmount(itemType).ToIntSafe();
+   
+                int amountToTransfer = targetAmmount - currentCount;
+                if (amountToTransfer < 1) { return true; } // Item already stocked
+
+                MyInventoryItem? item = source.FindItem(itemType);
+                if(item != null)
+                {
+                    return Inventory.TransferItemFrom(source, (MyInventoryItem) item, amountToTransfer);
+                }
+
+                return false;
+            }
+
+
+            // Add item type and count based on string from Loadout list.
+            private void AddTypeAndCount(string input, List<MyItemType> itemTypes)
+            {
+                string[] data = input.Split(':');
+
+                if (data.Length < 2) { return; }
+
+                string key = data[0];
+
+                try
+                {
+                    MyItemType itemType = ItemTypeFromString(key, itemTypes);
+
+                    ItemTypes.Add(key, itemType);
+
+                    int count;
+                    if (int.TryParse(data[1], out count))
+                        ItemAmmounts.Add(key, count);
+                    else
+                        ItemAmmounts.Add(key, 0);
+                }
+                catch { return; }
+            }
+
+
+            private MyItemType ItemTypeFromString(string itemString, List<MyItemType> itemTypes)
+            {
+                string itemName = itemString.ToLower();
+
+                foreach (MyItemType itemType in itemTypes)
+                {
+                    if (itemType.ToString().ToLower().Contains(itemName))
+                        return itemType;
+                }
+                throw new Exception("Could not locate item in type list");
+            }
+        }
+
+        public enum DestType { MAGAZINE, CONSTRUCTION, REACTOR, ICEBOX, ROCKBOX }
+    }
+
+
+
+}
---
+++ Clipboard_MDK2/Clipboard_MDK2/Program.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using VRage;
+using VRage.Collections;
+using VRage.Game;
+using VRage.Game.Components;
+using VRage.Game.GUI.TextPanel;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ModAPI.Ingame.Utilities;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRageMath;
+
+namespace IngameScript
+{
+    public partial class Program : MyGridProgram
+    {
+        #region MDK2 INFO
+        // This file contains your actual script.
+        //
+        // You can either keep all your code here, or you can create separate
+        // code files to make your program easier to navigate while coding.
+        //
+        // Go to:
+        // https://github.com/malware-dev/MDK-SE/wiki/Quick-Introduction-to-Space-Engineers-Ingame-Scripts
+        //
+        // to learn more about ingame scripts.
+        #endregion
+
+        #region Keep these variables
+        static IMyTextSurface _surface;
+        //MyIni _ini;
+        static IMyProgrammableBlock _Me;
+        #endregion
+
+        #region Test Variables (Can be deleted)
+        List<DestInventory> _magazines;
+
+        const string INI_HEAD = "USAP";
+        const string MAG_TAG = "[MAG";
+        const string AMMO_SUPPLY = "[WEP]"; // Source inventory tag for rearming.
+        const string LOADOUT = "Loadout";
+        const string GATLING = "GATLING";
+        const string MISSILE = "MISSILE";
+        const string ARTILLERY = "ARTILLERY";
+        const string ASSAULT = "ASSAULT";
+        const string AUTO = "AUTO";
+        const string RAIL = "RAIL";
+        const string MINI_RAIL = "MINI-RAIL";
+
+        #endregion
+
+        public Program()
+        {
+            // Set static instance of this program block
+            _Me = Me;
+
+            #region Data Screen Init
+            //_ini = GetIni(Me);
+            _surface = Me.GetSurface(0);
+            _surface.ContentType = ContentType.TEXT_AND_IMAGE;
+
+
+
+            #endregion
+
+            //List<IMyProjector> projectors = new List<IMyProjector>();
+            //GridTerminalSystem.GetBlocksOfType<IMyProjector>(projectors);
+
+
+
+            Build();
+
+
+            Runtime.UpdateFrequency = UpdateFrequency.Update100;
+        }
+
+        public void Save()
+        {
+            // Called when the program needs to save its state. Use
+            // this method to save your state to the Storage field
+            // or some other means. 
+            // 
+            // This method is optional and can be removed if not
+            // needed.
+        }
+
+        public void Main(string argument, UpdateType updateSource)
+        {
+            if (!string.IsNullOrEmpty(argument))
+            {
+                _surface.WriteText(updateSource.ToString() + "\nCMD: " + argument + "\n");
+
+                switch (argument.ToUpper())
+                {
+                    case "REFRESH":
+                            Build();
+                            break;
+                    case "REARM":
+                        ReloadAmmo();
+                        break;
+                    default:
+                        _surface.WriteText("UNRECOGNIZED COMMAND", true);
+                        break;
+                }
+            }
+
+        }
+
+
+        public void Build()
+        {
+            //_surface.WriteText("");
+
+            //_programIni = GetIni(Me);
+            _programIniHandler = new MyIniHandler(Me);
+            _gridID = _programIniHandler.GetKey(SHARED, "Grid_ID", Me.CubeGrid.EntityId.ToString());
+
+            AddMagazines();
+        }
+
+
+        public void AddMagazines()
+        {
+            _magazines = new List<DestInventory>();
+            List<IMyTerminalBlock> mags = new List<IMyTerminalBlock>();
+            GridTerminalSystem.SearchBlocksOfName(MAG_TAG, mags);
+
+            if (mags.Count < 1) { return; }
+
+            foreach(IMyTerminalBlock mag in mags)
+            {
+                SetMagAmounts(mag);
+
+                if (SameGridID(mag))
+                {
+                    DestInventory magazine = new DestInventory(DestType.MAGAZINE, mag);
+                    magazine.SetLoadout();
+
+                    _magazines.Add(magazine);
+                }
+            }
+        }
+
+
+        // SET MAG AMMOUNTS //
+        public void SetMagAmounts(IMyTerminalBlock block)
+        {
+            string name = block.CustomName;
+            if ((!name.Contains(MAG_TAG + "]") && !name.Contains(":")) || GetKey(block, SHARED, "Grid_ID", _gridID) != _gridID)
+                return;
+
+            string tag = TagFromName(name);
+            string loadout = "";
+
+            switch (tag.ToUpper())
+            {
+                case "GENERAL":
+                    loadout = "NATO_25x184mm:1\n" +
+                              "Missile200mm:1\n" +
+                              "LargeCalibreAmmo:1\n" +
+                              "MediumCalibreAmmo:1\n" +
+                              "AutocannonClip:1\n" +
+                              "LargeRailgunAmmo:1\n" +
+                              "SmallRailgunAmmo:1";
+                    break;
+                case GATLING:
+                    loadout = "NATO_25x184mm:7";
+                    break;
+                case MISSILE:
+                    loadout = "Missile200mm:4";
+                    break;
+                case ARTILLERY:
+                    loadout = "LargeCalibreAmmo:3";
+                    break;
+                case ASSAULT:
+                    loadout = "MediumCalibreAmmo:2";
+                    break;
+                case AUTO:
+                    loadout = "AutocannonClip:5";
+                    break;
+                case RAIL:
+                    loadout = "LargeRailgunAmmo:1";
+                    break;
+                case MINI_RAIL:
+                    loadout = "SmallRailgunAmmo:6";
+                    break;
+            }
+
+            EnsureKey(block, INI_HEAD, LOADOUT, loadout);
+        }
+
+        // TAG FROM NAME //  Gets specific tag from MAG name.
+        public string TagFromName(string name)
+        {
+            string tag = "";
+            if (name.Contains(MAG_TAG + "]"))
+                return "GENERAL";
+
+            int start = name.IndexOf(MAG_TAG) + MAG_TAG.Length + 1; //Start index of tag substring - includes colon
+            Echo("Start: " + start);
+            tag = name.Substring(start);
+            Echo(tag);
+            int length = tag.IndexOf("]");
+            Echo("Length: " + length);
+            tag = tag.Substring(0, length);//Length of tag
+            Echo(tag);
+
+            return tag;
+        }
+
+
+        public void ReloadAmmo()
+        {
+            if(_magazines.Count < 1)
+            {
+                _surface.WriteText("No Magazines to reload\n", true);
+            }
+
+            List<IMyInventory> sources = GetSourceInventories(AMMO_SUPPLY);
+            if(sources.Count < 1)
+            {
+                _surface.WriteText("No source inventories located\n", true);
+                return;
+            }
+
+            foreach (DestInventory mag in _magazines)
+            {
+                if (mag.Refill(sources))
+                    _surface.WriteText("RELOADED: " + mag.Block.CustomName + "\n", true);
+                else
+                    _surface.WriteText("RELOAD ERROR: " + mag.Block.CustomName + "\n", true);
+            }
+        }
+
+        public List<IMyInventory> GetSourceInventories(string tag)
+        {
+            List<IMyInventory> sources = new List<IMyInventory>();
+            List<IMyTerminalBlock> sourceBlocks = new List<IMyTerminalBlock>();
+
+            GridTerminalSystem.SearchBlocksOfName(AMMO_SUPPLY, sourceBlocks);
+
+            if (sourceBlocks.Count < 1) { return sources; }
+
+            foreach (IMyTerminalBlock block in sourceBlocks)
+            {
+                if(block.HasInventory)
+                    sources.Add(block.GetInventory());
+            }
+
+            return sources;
+        }
+    }
+}
